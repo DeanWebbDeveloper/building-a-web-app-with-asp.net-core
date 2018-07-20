6be11087bc175cd5c2058fb2c183ab0d3fad35a0
DeanWebbDeveloper <dean@deanwebbdeveloper.com>
2018-07-20 16:02:04 +0100

    Chapter 5
    Chapter 5 Completed

---
+++ 5 - Using ASP.NET MVC/5.9 - Implementing a Contact Page/DutchTreat/DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(object model)
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
+    }
+}
