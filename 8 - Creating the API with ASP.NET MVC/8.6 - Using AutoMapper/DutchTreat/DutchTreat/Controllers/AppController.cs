960fcce15b5d55d9f6905f402f11379b67abe52d
DeanWebbDeveloper <dean@deanwebbdeveloper.com>
2018-07-25 15:33:30 +0100

    Chapter 8
    Chapter 8 completed

---
+++ 8 - Creating the API with ASP.NET MVC/8.6 - Using AutoMapper/DutchTreat/DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,69 @@
+﻿using DutchTreat.Data;
+using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService mailService;
+        private readonly IDutchRepository _repository;
+
+        public AppController(IMailService mailService, IDutchRepository repository)
+        {
+            this.mailService = mailService;
+            this._repository = repository;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Send the email
+                this.mailService.SendMessage("dean@deanwebbdeveloper.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+            else
+            {
+                // Show the errors
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            var results = _repository.GetAllProducts();
+
+            return View(results);
+        }
+
+    }
+}
