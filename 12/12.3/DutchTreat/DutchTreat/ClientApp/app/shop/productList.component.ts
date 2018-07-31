e155bd94d8a50b07d195bd6c72e71d8061dbd936
DeanWebbDeveloper <dean@deanwebbdeveloper.com>
2018-07-31 16:19:29 +0100

    Chapter 12
    Chapter 12 - Started, up to lecture 5

---
+++ 12/12.3/DutchTreat/DutchTreat/ClientApp/app/shop/productList.component.ts
@@ -0,0 +1,27 @@
+﻿import { Component, OnInit } from "@angular/core";
+import { DataService } from "../shared/dataService";
+
+@Component({
+    selector: "product-list",
+    templateUrl: "productList.component.html",
+    styles: []
+})
+
+export class ProductList implements OnInit {
+
+    constructor(private data: DataService) {
+        this.products = data.products;
+    }
+
+    public products = [];
+
+    ngOnInit(): void {
+        this.data.loadProducts()
+            .subscribe(success => {
+                if (success) {
+                    this.products = this.data.products;
+                }
+            });
+    }
+
+}
